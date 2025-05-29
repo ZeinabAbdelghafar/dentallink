65777d5ddef1ca7f3d243ce983a710041ba3b76e
Engy Ahmed <engya306@gmail.com>
2025-05-29 21:45:14 +0300

    Add settings management with HomePageSettings class and database migration

---
+++ app/Settings/HomePageSettings.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Settings;
+
+use Spatie\LaravelSettings\Settings;
+
+class HomePageSettings extends Settings
+{
+    public string $home_title;
+    public string $home_subtitle;
+    public ?string $home_banner;
+
+    public static function group(): string
+    {
+        return 'home';
+    }
+}
