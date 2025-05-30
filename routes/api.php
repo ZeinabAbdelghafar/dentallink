94a7c00c449ad02ec4111eab3b8dc39784fcf59c
Engy Ahmed <engya306@gmail.com>
2025-05-30 18:47:05 +0300

    Add wishlist routes and secure cart routes with authentication middleware

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,19 @@
 use App\Http\Controllers\CartController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WishlistController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 
-Route::apiResource('cart', CartController::class);
+Route::middleware('auth:sanctum')->apiResource('cart', CartController::class);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/wishlist', [WishlistController::class, 'index']);
+    Route::post('/wishlist/toggle', [WishlistController::class, 'toggleFavorite']);
+    Route::post('/wishlist', [WishlistController::class, 'store']);
+    Route::put('/wishlist/{productId}', [WishlistController::class, 'update']);
+    Route::delete('/wishlist', [WishlistController::class, 'destroy']);
+});
